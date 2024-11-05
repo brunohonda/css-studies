3bef20024f75567d99ce889aede32b7bb3f24dcf
Bruno Honda <bruno.honda@live.com>
2024-11-05 14:16:47 -0300

    Implements open close menu behavior

---
+++ initial-menu/scripts.js
@@ -0,0 +1,20 @@
+function toggleMenu(el) {
+  if (el.classList.contains('open')) {
+    return el.classList.remove('open');
+  }
+
+  return el.classList.add('open');
+}
+
+(() => {
+  const menuButton = document.getElementById('menu-button');
+  const menu = document.getElementById('menu');
+  
+  menuButton.addEventListener('click', () => toggleMenu(menu));
+
+  document.addEventListener('keyup', (event)=> {
+    if(event.ctrlKey && event.key === 'Meta') {
+      menuButton.click();
+    }
+  });
+})()
